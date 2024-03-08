90c59304a0f97edd10523b5e6e880af89f9d96a7
Nguyễn Tiến Hoàng <hoangleo1303@gmail.com>
2024-03-08 09:30:28 +0700

    initial commit, done add function and can display the content now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { todowork } from './works';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'my-todo-list';
+export class AppComponent{
+  title = 'To-do list';
+  todoworks!: todowork[];
+  constructor(private cdr: ChangeDetectorRef){
+    this.todoworks = [];
+  };
+  addWork() {
+    var input= (<HTMLInputElement>document.getElementById("task-input")).value;
+    console.log(input);
+    this.todoworks.push({content:input,completed:true});
+    this.cdr.detectChanges();
+  };  
 }
