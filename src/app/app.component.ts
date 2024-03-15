d315b12e6ba83ba29b509c7b0902c2d41c29a2b2
Nguyễn Tiến Hoàng <hoangleo1303@gmail.com>
2024-03-15 17:52:51 +0700

    Nav bar finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, Injectable } from '@angular/core';
+import { Component, ChangeDetectorRef, Injectable, OnInit } from '@angular/core';
 import { todowork } from './works';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -11,58 +11,76 @@ import { AllWorkComponent } from './all-work/all-work.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-@Injectable({providedIn:'root'})
-export class AppComponent{
+@Injectable({ providedIn: 'root' })
+export class AppComponent {
   title = 'TODO LIST';
   todoworks!: todowork[];
-  constructor(private cdr: ChangeDetectorRef){
+  constructor(private cdr: ChangeDetectorRef) {
     this.todoworks = [];
   };
   addWork() {
-    var input= (<HTMLInputElement>document.getElementById("task-input")).value;
-    this.todoworks.push({content:input,completed:false});
+    var input = (<HTMLInputElement>document.getElementById("task-input")).value;
+    this.todoworks.push({ content: input, completed: false });
     (<HTMLInputElement>document.getElementById("task-input")).value = '';
     this.cdr.detectChanges();
   };
-  deleteWork(index: number){
-    this.todoworks.splice(index,1);
+  deleteWork(index: number) {
+    this.todoworks.splice(index, 1);
     this.cdr.detectChanges();
   };
-  completeWork(index: number){
-    if (this.todoworks[index].completed == false)
-    {
-    this.todoworks[index].completed= true;
+  completeWork(index: number) {
+    if (this.todoworks[index].completed == false) {
+      this.todoworks[index].completed = true;
     }
-    else
-    {
+    else {
       this.todoworks[index].completed = false
     }
     this.cdr.detectChanges();
   }
-  jobLeft(){
+  jobLeft() {
     let jobs: number = 0;
-    for (const element of this.todoworks){
-      if (element.completed == false){
-        jobs=jobs+1;
+    for (const element of this.todoworks) {
+      if (element.completed == false) {
+        jobs = jobs + 1;
       }
     }
     return jobs;
   }
-  sizeOfTodoworks(){
+  sizeOfTodoworks() {
     return this.todoworks.length;
-  } 
-  getTodoWork(){
+  }
+  getTodoWork() {
     return this.todoworks;
   }
-  clearCompletedWork(){
-    for (let i=0; i< this.todoworks.length; i++){
-      if (this.todoworks[i].completed == true){
+  clearCompletedWork() {
+    for (let i = 0; i < this.todoworks.length; i++) {
+      if (this.todoworks[i].completed == true) {
         this.deleteWork(i);
       }
     }
   }
-  editWork(index:number,newWork:string){
+  editWork(index: number, newWork: string) {
     this.todoworks[index].content = newWork;
+  }
+
+  navBarChange(index: string) {
+    switch (index) {
+      case "all":
+        document.getElementById("all").classList.add("selected");
+        document.getElementById("active").classList.remove("selected");
+        document.getElementById("completed").classList.remove("selected");
+        break;
+      case "active":
+        document.getElementById("all").classList.remove("selected");
+        document.getElementById("active").classList.add("selected");
+        document.getElementById("completed").classList.remove("selected");
+        break;
+      case "completed":
+        document.getElementById("all").classList.remove("selected");
+        document.getElementById("active").classList.remove("selected");
+        document.getElementById("completed").classList.add("selected");
+        break;
+    }
   }
 }
 
