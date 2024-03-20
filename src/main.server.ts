f8ba2ed8019667a62e325d06f7a2f949ff0980f1
Nguyễn Tiến Hoàng <hoangleo1303@gmail.com>
2024-03-20 13:29:28 +0700

    temp fix for LocalStorage

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-import 'localstorage-polyfill'
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 
-global['localStorage'] = localStorage;
 export default bootstrap;
