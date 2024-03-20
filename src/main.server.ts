b18db8effbc887175d9ba0ea1fb66f0835a4387a
Nguyễn Tiến Hoàng <hoangleo1303@gmail.com>
2024-03-20 14:24:24 +0700

    remove global variable localStorage

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-import 'localstorage-polyfill'
 const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-
-global['localStorage'] = localStorage;
 export default bootstrap;
